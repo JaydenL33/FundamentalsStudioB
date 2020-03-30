3e65141f58428bb8f51f0de32e18e26a87ccd1d0
Jayden Lee <Jayden@rlc.com.au>
2020-03-30 16:44:04 +1100

    simple css webpage
    this is a simple webpage display

--- src/backend/core/app.py
+++ src/backend/core/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def root():
+    return app.send_static_file('hello.html')
 
 if __name__ == "__main__":
   app.run(host="134.122.104.123")
