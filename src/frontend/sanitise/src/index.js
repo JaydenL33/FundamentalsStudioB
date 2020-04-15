2301d0b988024f4450d4f80747920bc294fbb195
Jayden Lee <Jayden@rlc.com.au>
2020-04-15 21:05:55 +1000

    Really Simple React dynamic data access

--- src/frontend/sanitise/src/index.js
+++ src/frontend/sanitise/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const title = React.createElement(
-	'h1',
-	{id: 'title', className: 'header'},
-	'Hello World'
+class Message extends React.Component {
+	render() {
+		console.log(this.props.msg);
+		return (
+			<div>
+				<h1 style={{color: this.props.color}}>
+					{this.props.msg}</h1>
+				<p> I'll check back in {this.props.minutes} minutes</p>
+			</div>
+		)
+	}
+}
 
+
+ReactDOM.render(<Message msg="How are you?" color="Blue" minutes={5}/>, 
+	document.getElementById('root')
 )
-ReactDOM.render(title,
-	document.getElementbyId('root'))
+
