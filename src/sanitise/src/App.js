169cb37a2dfa4572b51b33aec287b4defc44f1d8
Jayden Lee <Jayden@rlc.com.au>
2020-05-04 23:34:29 +1000

    React with sample HTML and CSS

--- src/sanitise/src/App.js
+++ src/sanitise/src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+var bop = "WHAT AN ABSOLUTE BOP!"
+
+function App () {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>This is just some Rendering with React. More to come! Oh and btw {bop}</h1>
     </div>
-  );
-}
+    )
+};
 
 export default App;
