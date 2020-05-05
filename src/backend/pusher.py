99ab720c047c20cb61f47b2313a1a8ca292fc7a1
theQuantumMechanic <albertferguson118@gmail.com>
2020-05-06 03:34:29 +1000

    Update pusher.py
    Cleaned and refactored, now runs robust startup that default pushes all frames outputed from cleaner

--- src/backend/pusher.py
+++ src/backend/pusher.py
@@ -2,8 +2,13 @@ __doc__="""Takes dataframes completed by overviewer and pushes them to the centr
 data schema. Guarantees correct typing and secure connection.
 """
 
-# core
+# core user lib
 from core import environConfig
+
+
+# Python core
+import os
+import pickle
 
 # third party libs
 import pandas as pd
@@ -11,27 +16,73 @@ import sqlalchemy
 from sqlalchemy.types import Integer, Text, String, DateTime, Numeric, Float, Boolean
 import numpy as np
 
-# python core
-import pickle
-import os
-
-#################################################################################
+################################################################################
 # Globals
-env = environConfig.safe_environ()
+# Some defaults in case env var fails.
+################################################################################
 
-DEBUG = env.bool("DEBUG")
 RAW_DF_LIST = []
 RAWGLOBAL_DF_LIST = []
-baseDir = env.str("BASE_DATA_DIR")
+baseDir =  os.path.join(os.path.join(os.pardir, os.pardir), "data") # file name for Raw Data (defaults to my local machine as ../../data)
+DEBUG = False                                                      # verbose debug prints
+URI_str = ''
 
-#################################################################################
+################################################################################
+# Utilities
+################################################################################
+
+def _runStartup():
+	"""
+	Author: Albert Ferguson
+	Explicit startup function. Runs any necessary preloads for data and Globals updates.
+	"""
+	# env = environConfig.safe_environ()
+
+	global DEBUG
+	global RAW_DF_LIST
+	global RAWGLOBAL_DF_LIST
+	global baseDir
+
+	# update with the env config
+	# DEBUG = env("DEBUG")
+	# baseDir = env("BASE_DATA_DIR")
+
+	try:
+		# call the read binary to read-in the data frames RAW
+		with open(os.path.join(baseDir, "processing_dump.txt"), "rb") as f:
+			RAW_DF_LIST = pickle.load(f)
+
+		# call the read binary to read-in the global data frames RAW
+		with open(os.path.join(baseDir, "globaldata_processing_dump.txt"), "rb") as f:
+			RAWGLOBAL_DF_LIST = pickle.load(f)
+
+	except FileNotFoundError:
+		return False
+	
+	except AttributeError:
+		return False
+
+	return True
 
 def dbConnect():
-	# pull sensitive settings from local.env for database login
-	env = environConfig.safe_environ()
+	"""
+	Author: Albert Ferguson
+	Create a new connection to the desired db engine. This setting is
+	controlled via the environ settings according to 12 Factor methods.
+	
+	Returns: engine, the db engine
+	"""
+
+	global URI_str
 	URI_str = env("DB_URI")
+	
 	engine = sqlalchemy.create_engine(URI_str)
+
 	return engine
+
+################################################################################
+# Database interactors
+################################################################################
 
 def pushFrame(df, connex, name, schema):
 	"""Write records stored in a DataFrame to a SQL database.
@@ -43,7 +94,6 @@ def pushFrame(df, connex, name, schema):
 	"""
 
 	def _convertDtypes(df):
-		def _switcher(arg):
 		switcher = {
 			np.dtype("int64"): Integer,
 			np.dtype("float64"): Float,
@@ -55,9 +105,9 @@ def pushFrame(df, connex, name, schema):
 		cols_list = df.columns.tolist()
 		types_list = df.dtypes.tolist()
 
-		for i in range(len(types_list)):
-			types_list[i] = switcher.get(types_list[i], "TEXT")
-
+		# use our type dict switcher to switch on the type and swap them for verbose ones
+		for i in range(len(types_list)): types_list[i] = switcher[types_list[i]]
+		# dict cast the zip of verbose type lists
 		return dict(zip(cols_list, types_list))
 
 	_con = connex
@@ -76,32 +126,43 @@ def pushFrame(df, connex, name, schema):
                           method=_method)
 		#df.to_sql(name, con=_con, dtype=_dtype)
 		print(connex.execute("SELECT * FROM {}.{}".format(schema, name)).fetchall()[:10])
-		return True
+		
+		return True # only valid exit point
 
-	except ValueError as e:
-	# print(e)
+	except ValueError:
 		return False
+
 	return False
 
-
-# public interface
 def push(df, name, opt):
 	"""Note, schema is the db to write to within our MySQL storage."""
 	return pushFrame(df, dbConnect(), name, opt)
 
-# test
-with open(os.path.join(baseDir, "processing_dump.txt"), "rb") as procData:
-	RAW_DF_LIST = pickle.load(procData)
-with open(os.path.join(baseDir, "globaldata_processing_dump.txt"), "rb") as globData:
-	RAWGLOBAL_DF_LIST = pickle.load(globData)
+def pushAll():
+	try:
+		for df in RAW_DF_LIST:
+			print(df["schema"].iloc[0])
+			if not push(df, df["schema"].iloc[0], "rawdata"):
+				return False
 
-print("\n\nDF INFO\n{}\t{}\n".format(len(RAW_DF_LIST), len(RAWGLOBAL_DF_LIST)))
-input()
+		for df in RAWGLOBAL_DF_LIST:
+			print(df["schema"].iloc[0])
+			if not push(df, df["schema"].iloc[0]+"_GLOBAL",  "rawdata"):
+				return False
 
-for df in RAW_DF_LIST:
-	print(df["schema"].iloc[0])
-	push(df, df["schema"].iloc[0], "rawdata")
+	except AttributeError:
+		return False
 
-for df in RAWGLOBAL_DF_LIST:
-	print(df["schema"].iloc[0])
-	push(df, df["schema"].iloc[0]+"_GLOBAL",  "rawdata")
+	return True
+
+################################################################################
+# Main
+################################################################################
+
+if not _runStartup():
+	print("UNCAUGHT STARTUP ERROR.")
+
+if DEBUG:
+	print("\n\nDF INFO\n{}\t{}\n".format(len(RAW_DF_LIST), len(RAWGLOBAL_DF_LIST)))
+
+pushAll() # atm just push all, this script isn't called otherwise
