27bf4222016950978ebbb872e73e09b9534669aa
CocoBozzy <cohenbos@gmail.com>
2020-05-09 15:48:44 +1000

    Update timeline.py

--- data/further_data_mining/experimentation_views/timeline.py
+++ data/further_data_mining/experimentation_views/timeline.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.dates as mdates
 from datetime import datetime
+import sklearn
 
 try:
     # Try to fetch a list of Matplotlib releases and their dates
