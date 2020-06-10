3163b5571d46c86962f13b8fbfac32678c4e325d
Joel-M3 <50311672+Joel-M3@users.noreply.github.com>
2020-06-10 12:14:11 +1000

    Scroll- Odd Code Testing
    Testing Code - Ignore

--- src/sanitise/src/pages/Indicators.js
+++ src/sanitise/src/pages/Indicators.js
@@ -1,17 +1,86 @@
 import React, { Component } from 'react';
 import Navbar from '../components/navbar';
+import './Indicators.css';
+import B11 from '../images/building11.png'
 
 
 
+const images = {
+    position: 'sticky',
+    height: '600px',
+    top: '0',
+    width: 'cover',
+    zIndex: '0',
+}
+
+
+/*        <img src={B11} style ={imagestyle} alt="logo" ></img>*/
 class Indicators extends Component {
     render () {
         return (
             <div>
             <Navbar />
-            </div>
+
+
+ 
+            <img src={B11} style ={images} alt="logo" ></img>
+
+              <div className ="inside">  
+             
+          
+     
+                <p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                maiestatis persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                maiestatis persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                maiestatis persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                maiestatis persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                maiestatis persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+                maiestatis persequeris pro, pri ponderum tractatos ei.
+                orem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat 
+                phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum
+          
+
+                </p>
+
+
+    
+              </div>
+
+
+              
+
+
+
+
+
+                </div>
+
+
         );
     }
 }
-
 
 export default Indicators;
